2753d33f684d5ae643d049afc5245c3e24d68587
stephenwang99 <stephenwang919@gmail.com>
2018-02-08 22:43:30 +1100

    updated after review

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,46 +4,80 @@ import media
 import fresh_tomatoes
 
 
-
-# create the function to get moive's information via omdbapi database by movieid
-
 def getdata(movieid, youtubeid):
-    res = requests.get('http://www.omdbapi.com/?i={}&apikey=46f7964d'.format(movieid))
+    """ the function obtain the moive information via api website.
+
+    Args:
+        movieid (str): global moive's id.
+        youtubeid (str): video's id in Youtube.
+
+    Returns:
+        Return a tuple of moive's value.
+
+    """
+    res = requests.get('http://www.omdbapi.com/?i={}&'
+                       'apikey=46f7964d'.format(movieid))
     jd = json.loads(res.text.strip('var data='))
     name = jd['Title']
     year = jd['Year']
     rated = jd['Rated']
-    plot= jd['Plot']
+    plot = jd['Plot']
     image_url = jd['Poster']
     youtube_url = "https://www.youtube.com/watch?v={}".format(youtubeid)
     return (name, year, image_url, youtube_url, rated, plot)
-    
+
 # call the function get the information and saved in the list
-inf = getdata('tt3896198',"2cv2ueYnKjg")
-#create an instance of the moive
-galaxy2 = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+inf = getdata("tt3896198", "2cv2ueYnKjg")
+# create an instance of the moive
+galaxy2 = media.Movie(inf[0],
+                      inf[1],
+                      inf[2],
+                      inf[3],
+                      inf[4],
+                      inf[5])
 
 inf = getdata("tt3470600", "Wm7qEjEny0w")
-sing = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+sing = media.Movie(inf[0],
+                   inf[1],
+                   inf[2],
+                   inf[3],
+                   inf[4],
+                   inf[5])
 
 inf = getdata("tt0478970", "pWdKf3MneyI")
-ant_man = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+ant_man = media.Movie(inf[0],
+                      inf[1],
+                      inf[2],
+                      inf[3],
+                      inf[4],
+                      inf[5])
 
 inf = getdata("tt3501632", "ue80QwXMRHg")
-thor3 = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+thor3 = media.Movie(inf[0],
+                    inf[1],
+                    inf[2],
+                    inf[3],
+                    inf[4],
+                    inf[5])
 
 inf = getdata("tt2294629", "FLzfXQSPBOg")
-frozen = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+frozen = media.Movie(inf[0],
+                     inf[1],
+                     inf[2],
+                     inf[3],
+                     inf[4],
+                     inf[5])
 
 inf = getdata("tt3521164", "LKFuXETZUsI")
-moana = media.Movie(inf[0],inf[1],inf[2],inf[3],inf[4],inf[5])
+moana = media.Movie(inf[0],
+                    inf[1],
+                    inf[2],
+                    inf[3],
+                    inf[4],
+                    inf[5])
 
 # create the list of all the moive's name
 movies = [galaxy2, thor3, ant_man, sing, frozen, moana]
 
 # call the fresh tomatoes to open the website
 fresh_tomatoes.open_movies_page(movies)
-
-
-    
-    
--- media.py
+++ media.py
@@ -1,13 +1,30 @@
-import webbrowser
-
 class Movie():
-    def __init__(self, movie_title, movie_year, poster_image, trailer_youtube, movie_rated, movie_plot):
+
+    def __init__(self, movie_title, movie_year, poster_image, trailer_youtube,
+                 movie_rated, movie_plot):
+        """The __init_ method contains attributes of moive's information.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            movie_title (str): Moive's Name.
+            movie_year (int): Moive's release year.
+            poster_image (str): the url of image file of Poster
+            trailer_youtube (str): the url of the moive trailer in Youtube.
+            movie_rated (str): the rate of the movie.
+            movie_plot (str): the storyline of the movie.
+
+        """
         self.title = movie_title
         self.year = movie_year
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.rated = movie_rated
         self.plot = movie_plot
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
